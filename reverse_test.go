e7dfb8368ed04f7c691b50793cc7ac18a65285fc
Muriel Silveira <murielkong@gmail.com>
2016-05-03 23:45:10 -0300

    A simple failing test

--- reverse_test.go
+++ reverse_test.go
@@ -3,5 +3,12 @@ package gostringutil
 import "testing"
 
 func TestReverse(t *testing.T) {
+  input := "!oG olleG"
+  expected := "Hello Go!"
 
+  got := Reverse(input)
+
+  if got != expected {
+    t.Errorf("Reverse(%q) == %q, expected %q", input, got, expected)
+  }
 }
