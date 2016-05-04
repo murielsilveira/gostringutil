bfd04b903246ffe4848d2cd6550bd2030d57cfc2
Muriel Silveira <murielkong@gmail.com>
2016-05-04 00:02:38 -0300

    Use two different approaches on the test to practice and compare then

--- reverse_test.go
+++ reverse_test.go
@@ -2,7 +2,50 @@ package gostringutil
 
 import "testing"
 
-func TestReverse(t *testing.T) {
+func TestSimpleReverse(t *testing.T) {
+  input := "Hello Go!"
+  expected := "!oG olleH"
+
+  got := Reverse(input)
+
+  if got != expected {
+    t.Errorf("Reverse(%q) == %q, expected %q", input, got, expected)
+  }
+}
+
+func TestWithChineseCharacters(t *testing.T) {
+  input := "Hello 世界"
+  expected := "界世 olleH"
+
+  got := Reverse(input)
+
+  if got != expected {
+    t.Errorf("Reverse(%q) == %q, expected %q", input, got, expected)
+  }
+}
+
+func TestWithASingleCharacter(t *testing.T) {
+  input := "å"
+  expected := "å"
+
+  got := Reverse(input)
+
+  if got != expected {
+    t.Errorf("Reverse(%q) == %q, expected %q", input, got, expected)
+  }
+}
+
+func TestWithEmptyString(t *testing.T) {
+  input := ""
+
+  got := Reverse(input)
+
+  if got != input {
+    t.Errorf("Reverse(%q) == %q, expected %q", input, got, input)
+  }
+}
+
+func TestReverseWithAllCases(t *testing.T) {
   cases := []struct { input, expected string } {
     {"Hello Go!", "!oG olleH"},
     {"Hello 世界", "界世 olleH"},
